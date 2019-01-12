5b972c878a707d6104100ec46e63782d78260420
connoringold <connoringold@gmail.com>
2019-01-12 20:44:29 +0000

    create the client folder and vue app wioth basic pages

---
+++ client/src/views/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Contact me</h1>
+  </div>
+</template>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home text-center">
+    <div class="jumbotron">
+      <h1 class="display-3">Hello, world!</h1>
+      <p class="lead">
+        This is a simple hero unit.
+      </p>
+      <hr class="my-4">
+      <p>
+        It uses utility classes for typography and spacing to
+      </p>
+      <p class="lead">
+        <router-link class="btn btn-primary btn-lg"
+        :to="{ name:'signup' }"
+        role="button">Sign up</router-link>
+      </p>
+    </div>
+  </div>
+</template>
+ <!-- should match the name in the router -->
+<script>
+// @ is an alias to /src
+
+
+</script>
+
+<style>
+</style>
